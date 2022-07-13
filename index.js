de7ddcd623bd3bfde80135d4e347a7d2380ab0ef
Deependu_Jha <deependujha21@gmail.com>
2022-07-13 20:12:21 +0530

    added middleware for serving static files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./conn/mongo");
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 const {
   UserAccountInfo,
   UserOrderHistory,

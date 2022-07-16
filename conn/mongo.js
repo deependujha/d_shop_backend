38c968c4997131fb97bf852a769c6241ff9f7ae9
Deependu_Jha <deependujha21@gmail.com>
2022-07-16 22:28:24 +0530

    changed data base from mongodb atlas to compass

--- conn/mongo.js
+++ conn/mongo.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.5ocuc97.mongodb.net/?retryWrites=true&w=majority`
-  )
+.connect(`mongodb://localhost:27017/d-shop`)
+  // .connect(
+  //   `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.5ocuc97.mongodb.net/?retryWrites=true&w=majority`
+  // )
   .then(() => {
     // console.log(`Connected To Database !`);
   })
